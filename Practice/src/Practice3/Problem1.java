7d4f15716157899502685c5ffb9ea236b8be3508
DilyaraSuleimanova <suleimanovadilyara04@gmail.com>
2026-02-16 09:39:56 +0500

    Practice

---
+++ Practice/src/Practice3/Problem1.java
@@ -0,0 +1,52 @@
+package Practice3;
+
+class Animal {
+	private int legs;
+	private String name;
+	
+	public Animal(String name) {
+		this.legs = 4;
+		this.name = name;
+	}
+	
+	public void eat() {
+		System.out.println("Nom nom nom");
+	}
+	
+	public void eat(String food) {
+		System.out.println("Eating " + food);
+	}
+	
+	public void makesound() {
+		System.out.println("🔊");
+	}
+	
+}
+
+class Dog extends Animal {
+	
+	public Dog(String name) {
+		super(name);
+	}
+	
+	public void eat() {
+		super.eat();
+		System.out.println("Chunk chunk chunk");
+	}
+	
+	@Override 
+	public void makesound() {
+		System.out.println("Woof Woof");
+	}
+}
+
+public class Problem1 {
+	public static void main(String[] args) {
+		Dog dog = new Dog("Lessy");
+		
+		dog.eat();
+        dog.eat("meat"); 
+        
+        dog.makesound();
+	}
+}
